6bfa2f484c4a51775d2af6a8e596261a931346ca
anwar <saefulanwar7721@gmail.com>
2022-04-01 16:51:47 +0700

    menambahkan class <NAMA_CLASS>

---
+++ tpmodul6_kelompok_2/DataMahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace tpmodul6_kelompok_2
+{
+    class DataMahasiswa
+    {
+        void ReadJson()
+        {
+            //Baca file Json yang diinginkan
+            String jsonString = File.ReadAllText("D:\Tugas Kuliah\Semester IV\Konstruksi PL\Praktikum_kpl\TP\tpMod6\tpmodul6_kelompok_2 1302204065.json");
+            //Deserialize file json menjadi object
+            ContohClassDataMahasiswa DataMahasiswa =
+            JsonSerializer.Deserialize<ContohClassDataMahasiswa>(jsonString);
+            Console.WriteLine("Nama Depan :" + DataMahasiswa.nama);
+            Console.WriteLine("Nama Belakang :" + DataMahasiswa.nama)
+            Console.WriteLine("NIM :" + DataMahasiswa.nim);
+            Console.WriteLine("Fakultas :" + DataMahasiswa.fakultas);
+
+            
+        }
+    }
+}
+
--- tpmodul6_kelompok_2/Program.cs
+++ tpmodul6_kelompok_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace tpmodul6_kelompok_2 
+{ 
+    class Program 
+    {
+        static void Main(String[] args) 
+        {
+            DataMahasiswa objMhs= new DataMahasiswa("Saeful", "anwar", 1302204065, "SE-44-01");
+            //String readJson = File.ReadAllText(@"data.json");
+            TulisJsonFile(objMhs);
+        }
+
+        static void TulisJsonFile(DataMahasiswa input) 
+        {
+            string json = JsonConvert.SerializeObject(input);
+            Console.WriteLine(jsonString);
+
+            File.WriteAllText(D:\Tugas Kuliah\Semester IV\Konstruksi PL\Praktikum_kpl\TP\tpMod6\tpmodul6_kelompok_2 1302204065.json);
+        }
+    }
+}
